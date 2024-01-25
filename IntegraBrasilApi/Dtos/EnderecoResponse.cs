26c6b3cdbc9a324669812bb97259bdbaf223f888
will787 <wvmwill@gmail.com>
2024-01-25 14:53:18 -0300

    incluindo interface e modelo para request da api, com a api de Banco e tambem de endereco CEP

---
+++ IntegraBrasilApi/Dtos/EnderecoResponse.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegraBrasilApi.Dtos
+{
+    public class EnderecoResponse
+    {
+        public string? Cep { get; set; }
+        public string? Estado { get; set; }
+        public string? Cidade { get; set; }
+        public string? Regiao { get; set; }
+        public string? Rua { get; set; }
+        public string? Servico { get; set; }
+    }
+}
