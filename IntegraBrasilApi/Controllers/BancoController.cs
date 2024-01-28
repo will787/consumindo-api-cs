0aa6bd896aab7daad352ec0c911e5e864170aa7c
will787 <wvmwill@gmail.com>
2024-01-27 22:40:12 -0300

    implement controller comunication with aplication in protocol http code

--- IntegraBrasilApi/Controllers/BancoController.cs
+++ IntegraBrasilApi/Controllers/BancoController.cs
@@ -1,15 +1,34 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using IntegraBrasilApi.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IntegraBrasilApi.Controllers
 {
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class BancoControllers : ControllerBase
+    public class BancoController : ControllerBase
     {
-        
+        private readonly IBancoService _bancoService;
+
+        public BancoController(IBancoService bancoService)
+        {
+            _bancoService = bancoService;
+        }
+
+        [HttpGet("busca/{banco}")]
+        public async Task<ActionResult> BuscarBanco([FromRoute] string banco){
+            var response = await _bancoService.BuscarBanco(banco);
+            if(response.CodigoHttp == HttpStatusCode.OK){
+                return Ok(response.DadosRetorno);
+            }
+            else
+            {
+                return StatusCode((int) response.CodigoHttp, response.ErroRetorno);
+            }
+        }
     }
 }
--- IntegraBrasilApi/Program.cs
+++ IntegraBrasilApi/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSingleton<IEnderecoService, EnderecoService>();
+//builder.Services.AddSingleton<IEnderecoService, EnderecoService>();
 //builder.Services.AddSingleton<IBancoService, BancoService>();
 builder.Services.AddSingleton<IBrasilApi, BrasilApiRest>();
 
